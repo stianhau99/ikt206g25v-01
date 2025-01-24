0c2b83d92548c626cfc2a4b6492c3f197cd35367
Stian Haugen <stianhau@uia.no>
2025-01-24 11:55:56 +0100

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ikt206g25v_01.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
